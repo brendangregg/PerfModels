fb492cbbb13f73add8efa528b90ea58d54bb00c4
Brendan Gregg <brendan.gregg@joyent.com>
2013-06-18 16:57:31 -0700

    simpler technique

--- frequencytrail.r
+++ frequencytrail.r
@@ -7,33 +7,25 @@
 #
 # 08-Jun-2013	Brendan Gregg	Created this.
 
-#pdf("frequencytrailtest.pdf", w=8, h=4)
+pdf("frequencytrailtest.pdf", w=8, h=4)
 
 # plot a data frame as a frequency trail
 plotfrequencytrail <- function(data) {
-	n <- 2048			# resolution
-	thr <- 0.000001			# threshold
-	lwd <- 4			# line width
+	n <- 2048				# resolution
+	thr <- 1 / (sd(data) * length(data))	# threshold
+	lwd <- 4				# line width
 	den <- density(data, n=n)
 
-	maxy <- max(den$y)
-	px <- den$x[1]
-	py <- den$y[1]
-
-	# initialize x and y range
 	plot(den, col=NA, fg=NA)
 
-	for (i in 2:n) {
-		if (py > thr | den$y[i] > thr) {
-			lines(c(px, den$x[i]), c(py, den$y[i]), lwd=lwd)
-		}
-		py <- den$y[i]
-		px <- den$x[i]
-	}
+	# replace low frequency with NA to avoid plotting
+	for (i in 1:n) { if (den$y[i] < thr) { den$y[i] = NA } }
+
+	lines(den, lwd=lwd)
 }
 
 # data set is a normal distribution plus outliers
-data <- c(rnorm(9990, mean=1000, sd=200),
+data <- c(rnorm(9900, mean=1000, sd=100),
 	  runif(10, min=2000, max=10000))
 
 plotfrequencytrail(data)
